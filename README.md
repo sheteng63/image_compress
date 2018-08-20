5cfe34916eab05820f55b48e5e22570468a4ca2c
sheteng <sheteng@live.cn>
2018-08-20 13:46:29 +0800

    增加写到文件的方法

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ a simple jepg image compress plugins
 
 ## Install
 >image_compress:
->      git:
->        url: https://github.com/sheteng63/image_compress.git
+      git:
+        url: https://github.com/sheteng63/image_compress.git
 
 ## Example
 > import 'package:image_compress/image_compress.dart';
-> var memory = await ImageCompress.compressImageToMemory(path);
+ var memory = await ImageCompress.compressImageToMemory(path);
+
+
--- lib/image_compress.dart
+++ lib/image_compress.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-
 import 'package:flutter/services.dart';
 
 class ImageCompress {
@@ -17,6 +16,21 @@ class ImageCompress {
       {rate = 1, width = 500, height = 500}) async {
     return await _channel.invokeMethod("compressImage",
         {"path": path, "rate": rate, "width": width, "height": height});
+  }
+
+  /**
+   * path: the image path
+   * rate: compress rate range(0~1)
+   * width: new image width
+   * height: new image height
+   * return: new image memory
+   */
+  static Future compressImageToFile(String path,File file,
+      {rate = 1, width = 500, height = 500}) async {
+    var res = await _channel.invokeMethod("compressImage",
+        {"path": path, "rate": rate, "width": width, "height": height});
+    await (await file).writeAsString(res);
+
   }
 
 }
